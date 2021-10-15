4529ba2ad5f4030674fa8e3a254941593b97f02c
stantanasi <stantanasi@gmail.com>
2021-10-15 21:15:10 +0000

    Deploying to deployment from @ stantanasi/netflix-angular@cfc4eef428a7de37b749b804d143b453a95346bc 🚀

--- main.js
+++ main.js
@@ -175,7 +175,6 @@ __webpack_require__.r(__webpack_exports__);
 const environment = {
     production: false,
     apiKey: '966bc16006cd2667daf628d997d83afb',
-    apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjZiYzE2MDA2Y2QyNjY3ZGFmNjI4ZDk5N2Q4M2FmYiIsInN1YiI6IjYwMTQzNTRjYzhhMmQ0MDAzZmY0YzY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CmCAd4C8AYcAkx-YOTEVrSOghbkOPmIXKD1TwotFiSQ',
 };
 /*
  * For easier debugging in development mode, you can import the following file
