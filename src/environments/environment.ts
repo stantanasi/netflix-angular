cfc4eef428a7de37b749b804d143b453a95346bc
LoryStanTanasi <lory-stan.tanasi@42c.fr>
2021-10-15 23:14:01 +0200

    Remove TMDB API Token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 export const environment = {
   production: false,
   apiKey: '966bc16006cd2667daf628d997d83afb',
-  apiToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjZiYzE2MDA2Y2QyNjY3ZGFmNjI4ZDk5N2Q4M2FmYiIsInN1YiI6IjYwMTQzNTRjYzhhMmQ0MDAzZmY0YzY4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CmCAd4C8AYcAkx-YOTEVrSOghbkOPmIXKD1TwotFiSQ',
 };
 
 /*
